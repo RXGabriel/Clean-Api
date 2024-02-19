629bc7d6c0ddf14484ba5acec40875be90030072
RXGabriel <gabriel2005rf@gmail.com>
2024-02-19 15:58:35 -0300

    feat: ensure SignUpController returns 400 if an invalid email is provaded

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -2,7 +2,15 @@ import type { HttpResponse, HttpRequest } from '../protocols/http'
 import { MissingParamError } from '../errors/missing-param-error'
 import { badRequest } from '../helpers/http-helper'
 import { type Controller } from '../protocols/controller'
+import { InvalidParamError } from '../errors/invalid-param-error'
+import { type EmailValidator } from '../protocols/email-validator'
+
 export class SignUpController implements Controller {
+  private readonly emailvalidator: EmailValidator
+  constructor (emailValidator: EmailValidator) {
+    this.emailvalidator = emailValidator
+  }
+
   handle (httpRequest: HttpRequest): HttpResponse {
     const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
     for (const field of requiredFields) {
@@ -10,10 +18,11 @@ export class SignUpController implements Controller {
         return badRequest(new MissingParamError(field))
       }
     }
-
-    return {
-      statusCode: 200,
-      body: {}
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+    const isValid = this.emailvalidator.isValid(httpRequest.body.email)
+    if (!isValid) {
+      return badRequest(new InvalidParamError('email'))
     }
+    return badRequest(new InvalidParamError('Invalid request'))
   }
 }
