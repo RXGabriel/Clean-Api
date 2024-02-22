86d3180dd029f4e1695cb5de9315985b8de63de8
RXGabriel <gabriel2005rf@gmail.com>
2024-02-22 16:21:03 -0300

    test: ensure BcryptAdapter throws if bcrypt throws

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -4,9 +4,7 @@ import bcrypt from 'bcrypt'
 import { BcryptAdapter } from './bcrypt-adapter'
 
 jest.mock('bcrypt', () => ({
-  async hash (): Promise<string> {
-    return new Promise(resolve => resolve('hash'))
-  }
+  hash: jest.fn().mockResolvedValue('hash')
 }))
 
 const salt = 12
@@ -18,14 +16,20 @@ describe('Bcrypt Adapter', () => {
   test('Should call bcrypt with correct values', async () => {
     const sut = makeSut()
     const hashSpy = jest.spyOn(bcrypt, 'hash')
-    const valueToEncrypt = 'any_value'
     await sut.encrypt('any_value')
-    expect(hashSpy).toHaveBeenCalledWith(valueToEncrypt, salt)
+    expect(hashSpy).toHaveBeenCalledWith('any_value', salt)
   })
 
   test('Should return a hash on success', async () => {
     const sut = makeSut()
     const hash = await sut.encrypt('any_value')
     expect(hash).toBe('hash')
+  })
+
+  test('Should throw if bcrypt throws', async () => {
+    const sut = makeSut()
+    jest.spyOn(bcrypt, 'hash').mockRejectedValueOnce(new Error() as never)
+    const promise = sut.encrypt('any_value')
+    await expect(promise).rejects.toThrow()
   })
 })
