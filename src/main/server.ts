19037463d4e392c3cfe98b776dfb81d3d2370baf
RXGabriel <gabriel2005rf@gmail.com>
2024-02-26 15:04:23 -0300

    feat: ensure requests parse body as json

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,3 +1,3 @@
-import express from 'express'
-const app = express()
-app.listen(5050, () => console.log('Server running on port 5050'))
+import app from './config/app'
+
+app.listen(5050, () => { console.log('Server running on port 5050') })
